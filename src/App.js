6fe5197694d6cd5ad205db479d105f3f48835957
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2020-12-15 01:28:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "🥳": "party",
+  "😇": "blessed",
+  "😂": "funny",
+  "🤣": "intense laughter",
+  "😅": "close call",
+  "🥰": "Being in love",
+  "🤩": "impressive",
+  "😛": "just kidding"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+export default function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var meaning = emojiDictionary[event.target.value];
+    if (meaning === undefined && event.target.value !== "") {
+      meaning = "we don't have this in our database";
+    }
+    setEmojiMeaning(meaning);
+  }
+  function emojiReaction(emoji) {
+    setEmojiMeaning(emojiDictionary[emoji]);
+  }
+  return (
+    <div className="App">
+      <h1>Emojipreter</h1>
+      <input
+        onChange={emojiInputHandler}
+        className="textbox"
+        type="text"
+        placeholder="put an emoji here to know the meaning"
+      ></input>
+      <h2>{emojiMeaning}</h2>
+
+      <h3 style={{ paddingTop: "2rem" }}>emojis we know</h3>
+      {/* <p>{emojisWeKnow}</p> */}
+      <ul>
+        {emojisWeKnow.map(function (emoji, index) {
+          return (
+            <span
+              key={emoji}
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+              onClick={() => emojiReaction(emoji)}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
